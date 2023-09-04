203c5993bf8d3df9d636f0de4e7c0d99d276df17
Christinne <vanessa0christinne@gmail.com>
2023-09-04 10:26:00 +0200

    Add files via upload

---
+++ Pokedex/Pokedex/View/ContentView.swift
@@ -0,0 +1,32 @@
+import SwiftUI
+
+struct ContentView: View {
+	var body: some View {
+		TabView {
+			PokedexView()
+				.tabItem {
+					Image(systemName: "lanyardcard")
+					Text("Pokedex")
+				}
+			
+			BerriesView()
+				.tabItem {
+					Image(systemName: "tree")
+					Text("Berries")
+				}
+			
+			PokeballsView()
+				.tabItem {
+					Image(systemName: "cricket.ball.fill")
+					Text("Pokeballs")
+				}
+		}
+	}
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+		ContentView()
+			.colorScheme(.light)
+    }
+}
